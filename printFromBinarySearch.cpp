4bc424440a1d1d7e474aedb45aa38f462ab55394
Deeppradhan34 <deeppradhan34@gmail.com>
2017-06-14 18:19:08 +0530

    change in logic

--- printFromBinarySearch.cpp
+++ printFromBinarySearch.cpp
@@ -1,7 +1,7 @@
 #include<iostream>
 #include<algorithm>
 using namespace std;
-
+void PrintSmallestValue(int pairs[], int n, int sum);
 int print(int a[],int low, int high, int searchNum){// binary Search, now better time complexity nlogn
 	
 	int flag = 0;
@@ -26,6 +26,15 @@ int print(int a[],int low, int high, int searchNum){// binary Search, now better
 	return flag;
 }
 
+
+void PrintSmallestValue(int pairs[], int n, int sum){
+	int min = pairs[0];
+	for(int i = 1; i <n; i++){
+		if(pairs[i] < min)
+			min = pairs[i];
+   }
+   cout<<min<<" "<<(sum-min);
+}
 int main(){
 	int n;
 	cin>>n;
@@ -37,10 +46,13 @@ int main(){
 	cin>>sum;
 	int result;
 	sort(a,a+n);
+	int pairs[n];
+	int count = 0;
 	for(int i = 0; i<n; i++){
-			 int result  =  print(a,0,n-1,sum -a[i]);	
+		int result  =  print(a,0,n-1,sum -a[i]);	
 		if(result == 1)
-		cout<<a[i]<<" "<<(sum-a[i])<<" "<<"\n";
+			pairs[count++] = a[i];
 	}
+	PrintSmallestValue(pairs,count, sum);
 
 }
--- printSum.cpp
+++ printSum.cpp
@@ -1,16 +1,29 @@
 #include<iostream>
 using namespace std;
-
-void print(int a[], int n, int sum){// brute force
-	
+void PrintSmallestValue(int pairs[], int n, int sum);
+void getPairsOfSum(int a[], int n, int sum){// brute force
+	int pairs[n];
+	int count = 0;
 	for(int i = 0; i < n; i++){// Time Complexity is O(n2), not good.
 		for(int j = i+1; j <n; j++){
 			int sumIntegers  = a[i]+a[j];
 			if(sumIntegers == sum){
-				cout<<a[i]<<" "<<a[j]<<"\n";
+			    pairs[count++] = a[i];
+			    pairs[count++] =a[j];
+				
 			}
 		}
 	}
+	
+	PrintSmallestValue(pairs,count, sum);
+}
+void PrintSmallestValue(int pairs[], int n, int sum){
+	int min = pairs[0];
+	for(int i = 1; i <n; i++){
+		if(pairs[i] < min)
+			min = pairs[i];
+   }
+   cout<<min<<" "<<(sum-min);
 }
 
 int main(){
@@ -22,5 +35,5 @@ int main(){
 	
 	int sum;
 	cin>>sum;
-	print(a,n,sum);	
+	getPairsOfSum(a,n,sum);	
 }
